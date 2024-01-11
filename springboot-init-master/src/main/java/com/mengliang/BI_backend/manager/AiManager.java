110839a5fa6b8480af864df9e06044435d51344b
mengliang <13767029108@163.com>
2024-01-11 21:23:38 +0800

    用于对接 AI 平台

---
+++ springboot-init-master/src/main/java/com/mengliang/BI_backend/manager/AiManager.java
@@ -0,0 +1,44 @@
+package com.mengliang.BI_backend.manager;
+
+import com.mengliang.BI_backend.common.ErrorCode;
+import com.mengliang.BI_backend.exception.BusinessException;
+import com.yupi.yucongming.dev.client.YuCongMingClient;
+import com.yupi.yucongming.dev.common.BaseResponse;
+import com.yupi.yucongming.dev.model.DevChatRequest;
+import com.yupi.yucongming.dev.model.DevChatResponse;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 用于对接 AI 平台
+ */
+@Service
+public class AiManager {
+
+    @Resource
+    private YuCongMingClient yuCongMingClient;
+
+    /**
+     * AI 对话
+     *
+     * @param modelId
+     * @param message
+     * @return
+     */
+    // 在外层指定模型id，这样会优雅一点
+    public String doChat(long modelId, String message) {
+        // 第三步，构造请求参数
+        DevChatRequest devChatRequest = new DevChatRequest();
+        // 模型id，尾后加L，转成long类型
+        devChatRequest.setModelId(modelId);
+        devChatRequest.setMessage(message);
+        // 第四步，获取响应结果
+        BaseResponse<DevChatResponse> response = yuCongMingClient.doChat(devChatRequest);
+        // 如果响应为null，就抛出系统异常，提示“AI 响应错误”
+        if (response == null) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "AI 响应错误");
+        }
+        return response.getData().getContent();
+    }
+}
